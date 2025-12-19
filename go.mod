0f5083b2306ef22a653b6dbdd9a978036b32f714
MuhammadAbyaz <m.abyaz681@gmail.com>
2025-12-19 09:00:26 +0500

    init project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module serverless
+
+go 1.23.4
+
+require github.com/aws/aws-lambda-go v1.51.1 // indirect
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	if request.HTTPMethod == "GET" && request.Path == "" {
+	}
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello, World!",
+	}, nil
+}
+
+func main() {
+	lambda.Start(handleRequest)
+}
